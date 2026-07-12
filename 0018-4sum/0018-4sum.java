3437d48dd2c4726dcb61f086dc73a7f4f5ef943d
Deepanshu <dst0619@gmail.com>
2026-07-12 16:13:55 +0530

    Time: 21 ms (40.87%), Space: 45.9 MB (46.44%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,32 +1,61 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        Set<List<Integer>> set = new HashSet<>();
-        int n = nums.length;
+
         Arrays.sort(nums);
-        if(n<4) 
-        return new ArrayList<>(set);  
-        for(int i=0;i<n-3;i++)
-        {
-            for(int j=i+1;j<n-2;j++)
-            {
-                int left=j+1;
-                int right=n-1;
-                while(left<right)
-                {
-                    long sum = (long) nums[i] + (long) nums[j] + (long) nums[left] + (long) nums[right];
-                    if(sum==target)
-                    {
-                        set.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+
+        List<List<Integer>> ans = new ArrayList<>();
+
+        int n = nums.length;
+
+        for (int i = 0; i < n - 3; i++) {
+
+            // Skip duplicates
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+
+            for (int j = i + 1; j < n - 2; j++) {
+
+                // Skip duplicates
+                if (j > i + 1 && nums[j] == nums[j - 1])
+                    continue;
+
+                int left = j + 1;
+                int right = n - 1;
+
+                while (left < right) {
+
+                    long sum = (long) nums[i] + nums[j] + nums[left] + nums[right];
+
+                    if (sum == target) {
+
+                        ans.add(Arrays.asList(
+                                nums[i],
+                                nums[j],
+                                nums[left],
+                                nums[right]
+                        ));
+
                         left++;
                         right--;
+
+                        while (left < right && nums[left] == nums[left - 1])
+                            left++;
+
+                        while (left < right && nums[right] == nums[right + 1])
+                            right--;
+
+                    } else if (sum < target) {
+
+                        left++;
+
+                    } else {
+
+                        right--;
                     }
-                    else if(sum>target)
-                    right--;
-                    if(sum<target)
-                    left++;
                 }
             }
         }
-        return new ArrayList<>(set);    
+
+        return ans;
     }
 }
