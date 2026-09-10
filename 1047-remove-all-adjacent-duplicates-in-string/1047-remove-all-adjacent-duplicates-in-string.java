5fe5ba648e3e7afe49f16ad26150967928c1282a
Deepanshu <dst0619@gmail.com>
2026-09-10 22:01:16 +0530

    Time: 11 ms (91.28%), Space: 47.5 MB (18.87%) - LeetHub

---
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String removeDuplicates(String s) {
+        StringBuilder sb = new StringBuilder();
+        
+        for (char c : s.toCharArray()) {
+            int length = sb.length();
+            // If the current character matches the last added character, remove it
+            if (length > 0 && sb.charAt(length - 1) == c) {
+                sb.deleteCharAt(length - 1);
+            } else {
+                // Otherwise, append the current character
+                sb.append(c);
+            }
+        }
+        
+        return sb.toString();
+    }
+}
