8a492a2bcfbeaae2bd2c01118b1d4bb3ea1109ec
Deepanshu <dst0619@gmail.com>
2026-09-15 18:48:10 +0530

    Time: 3 ms (85.96%), Space: 43.6 MB (15.06%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -21,3 +21,40 @@ class Solution {
         return st.empty();
     }
 }
+
+
+
+// static class Solution {
+//     public boolean isValid(String s) {
+//         Stack<Character> st = new Stack<>();
+
+//         for (char ch : s.toCharArray()) {
+
+//             if (st.empty() || ch == '(' || ch == '{' || ch == '[') {
+//                 st.push(ch);
+//                 continue;
+//             }
+
+//             if (ch == ')') {
+//                 if (st.peek() == '(')
+//                     st.pop();
+//                 else
+//                     return false;
+//             } 
+//             else if (ch == '}') {
+//                 if (st.peek() == '{')
+//                     st.pop();
+//                 else
+//                     return false;
+//             } 
+//             else if (ch == ']') {
+//                 if (st.peek() == '[')
+//                     st.pop();
+//                 else
+//                     return false;
+//             }
+//         }
+
+//         return st.empty();
+//     }
+// }
